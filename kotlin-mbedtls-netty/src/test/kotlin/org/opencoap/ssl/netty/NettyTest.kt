ad19d99b2559c8695221b2d9d6f8040e60490745
Topias Jokiniemi <topias.jokiniemi@nordicsemi.no>
2023-06-13 16:31:10 +0300

    test fixed to use correct cid

--- kotlin-mbedtls-netty/src/test/kotlin/org/opencoap/ssl/netty/NettyTest.kt
+++ kotlin-mbedtls-netty/src/test/kotlin/org/opencoap/ssl/netty/NettyTest.kt
@@ -189,7 +189,7 @@ class NettyTest {
 
     @Test
     fun `server should load session from store`() {
-        sessionStore.write("f935adc57425e1b214f8640d56e0c733".decodeHex(), SessionWithContext(StoredSessionPair.srvSession, mapOf()))
+        sessionStore.write("059876266f7c5734fd352c5a3b7b3be2".decodeHex(), SessionWithContext(StoredSessionPair.srvSession, mapOf()))
         val storeSessionMock: SessionWriter = mockk(relaxed = true)
         val client = NettyTransportAdapter.reload(clientConf.loadSession(byteArrayOf(), StoredSessionPair.cliSession, srvAddress), srvAddress, storeSessionMock).mapToString()
 
