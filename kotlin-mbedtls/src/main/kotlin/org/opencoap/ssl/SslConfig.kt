476a9694191fe636c65538d954b8d1704ae0e537
Szymon Sasin <szymon.sasin@gmail.com>
2026-09-09 13:52:23 +0300

    impr

--- kotlin-mbedtls/src/main/kotlin/org/opencoap/ssl/SslConfig.kt
+++ kotlin-mbedtls/src/main/kotlin/org/opencoap/ssl/SslConfig.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2025 kotlin-mbedtls contributors (https://github.com/open-coap/kotlin-mbedtls)
+ * Copyright (c) 2022-2026 kotlin-mbedtls contributors (https://github.com/open-coap/kotlin-mbedtls)
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -250,7 +250,7 @@ data class PskAuth(
     override fun hashCode(): Int = 31 * pskId.contentHashCode() + pskSecret.contentHashCode()
 
     // pskSecret is never rendered
-    override fun toString(): String = "PskAuth(pskId=${pskId.toHex()}, pskSecret=<redacted>)"
+    override fun toString(): String = "PskAuth(pskId=${pskId.toHex()}, pskSecret=***)"
 }
 
 data class CertificateAuth(
